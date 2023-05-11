965436a3f7b69bedd523401ac15e79b13a273ce5
Marcel König <102319599+mkoenig3@users.noreply.github.com>
2023-05-11 10:32:31 -0700

    added qaa_cdom

--- bio_optics/models/qaa.py
+++ bio_optics/models/qaa.py
@@ -3,6 +3,7 @@ from .. helper.utils import find_closest
 from .. helper.resampling import resample_a_w, resample_b_bw
 from .. surface.air_water import above2below
 
+
 def qaa(R_rs, 
         wavelengths, 
         lambdas=np.array([412, 443, 490, 555, 640, 670]),
@@ -11,20 +12,19 @@ def qaa(R_rs,
         h0=-1.146,
         h1=-1.366,
         h2=-0.469,
-        j_1=0.63, 
-        j_2=0.88,
         a_w_res=[],
         b_bw_res=[]):
     """
-    Quasi-Analytical Algorithm (QAA) to derive the absorption and backscattering coefficients by analytically inverting the spectral remote-sensing reflectance [sr-1] [1,2,3],
-    and extended with CDOM estimation [4]. Only valid for optically deep water.
-    Note that the equations follow the IOCCG updates [2,3] and are not identical to the ones provided in [4].
+    Quasi-Analytical Algorithm (QAA) to derive the absorption and backscattering coefficients by analytically inverting the spectral remote-sensing reflectance [sr-1] [1,2,3]. 
+    Only valid for optically deep water.    
     The function can run with any number of bands as long as the crucial wavelengths (lambdas) are included.
+
+    I believe that there is a typo in the QAA_v6 documentation (https://www.ioccg.org/groups/Software_OCA/QAA_v6_2014209.pdf): in Steps 9 & 10, where the first line 
+    is a_g(443) while I think should be a_dg(443) as in the AQQ_v5 documentation (https://www.ioccg.org/groups/Software_OCA/QAA_v5.pdf).
  
     [1] Lee et al. (2002): Deriving inherent optical properties from water color: A multiband quasi-analytical algorithm for optically deep waters [10.1364/ao.41.005755]
     [2] Lee et al. (no year): An Update of the Quasi-Analytical Algorithm (QAA_v5). Available online: https://www.ioccg.org/groups/Software_OCA/QAA_v5.pdf
     [3] Lee et al. (2014): Update of the Quasi-Analytical Algorithm (QAA_v6). Available online: https://www.ioccg.org/groups/Software_OCA/QAA_v6_2014209.pdf
-    [4] Zhu & Yu (2013): Inversion of chromophoric dissolved organic matter from EO-1 hyperion imagery for turbid estuarine and coastal waters [10.1109/TGRS.2012.2224117]
 
     Args:
         R_rs: remote sensing reflectance [sr-1]
@@ -67,7 +67,6 @@ def qaa(R_rs,
     chi = np.log((r_rs[idx[1]] + r_rs[idx[2]]) / (r_rs[idx[3]] + 5*(r_rs[idx[5]]/r_rs[idx[2]]) * r_rs[idx[5]]))
     a_lambda_0_v5 = a_w_res[idx[3]] + 10**(h0 + h1*chi + h2*chi**2)
     a_lambda_0_v6 = a_w_res[idx[5]] + 0.39 * (R_rs[idx[5]] / R_rs[idx[1]] + R_rs[idx[2]])**1.14
-    # a_lambda_0 = np.where(R_rs[idx[5]] < 0.0015, a_lambda_0_v5, a_lambda_0_v6)
 
     # Step 3 [3]
     b_bp_lambda_0_v5 = ((u[idx[3]] * a_lambda_0_v5) / (1-u[idx[3]])) - b_bw_res[idx[3]]
@@ -87,16 +86,90 @@ def qaa(R_rs,
 
     # Step 7 & 8 [3]
     zeta = 0.74 + (0.2 / (0.8 + r_rs[idx[1]] / r_rs[idx[3]]))
-    S = 0.015 + (0.002 / (0.6 + r_rs[idx[1]] / r_rs[idx[3]]))
-    xi = np.exp(S*(442.5-415.5))
+    s = 0.015 + (0.002 / (0.6 + r_rs[idx[1]] / r_rs[idx[3]]))
+    xi = np.exp(s*(442.5-415.5))
 
     # Step 9 & 10 [3]
     a_dg_443 = ((a[idx[0]] - zeta*a[idx[1]]) / (xi - zeta)) - ((a_w_res[idx[0]] - zeta*a_w_res[idx[1]]) / (xi - zeta))
-    a_dg = np.array([a_dg_443*np.exp(-S*(lambda_i-wavelengths[idx[1]])) for lambda_i in wavelengths])
+    a_dg = np.array([a_dg_443*np.exp(-s*(lambda_i-wavelengths[idx[1]])) for lambda_i in wavelengths])
     a_ph = a - a_dg - a_w_res[idx[1]]
 
-    # CDOM estimation [4]
-    a_p_440 = j_1 * b_bp[idx[1]]**j_2
-    a_g_440 = a[idx[1]] - a_w_res[idx[1]] - a_p_440
+    return a, a_ph, a_dg, b_b, b_bp
 
-    return a, a_ph, a_dg, a_p_440, a_g_440, b_b, b_bp
+
+def qaa_cdom(R_rs, 
+             wavelengths, 
+             lambdas=np.array([440, 490, 555, 640]),
+             k0=6.807,
+             k1=1.186,
+             h0=-1.169,
+             h1=-1.468,
+             h2=0.274,
+             j1=0.63,
+             j2=0.88,
+             a_w_res=[],
+             b_bw_res=[]):
+    """
+    Quasi-Analytical Algorithm (QAA) to derive absorption of CDOM and particulate matter at a reference wavelength of 440 nm [1] based on the QAA_v5 algorithm [2,3]. 
+    Only valid for optically deep water.    
+
+    Zhu & Yu made some empirical adjustments based on the NOMAD data set. These adjustments (e.g., computation of u in Step 1) are incorporated in this function,
+    except for the Gamma adjustment in u(). 
+
+    [1] Zhu & Yu (2013): Inversion of chromophoric dissolved organic matter from EO-1 hyperion imagery for turbid estuarine and coastal waters [10.1109/TGRS.2012.2224117]
+    [2] Lee et al. (2002): Deriving inherent optical properties from water color: A multiband quasi-analytical algorithm for optically deep waters [10.1364/ao.41.005755]
+    [3] Lee et al. (no year): An Update of the Quasi-Analytical Algorithm (QAA_v5). Available online: https://www.ioccg.org/groups/Software_OCA/QAA_v5.pdf
+
+    Args:
+        R_rs: remote sensing reflectance [sr-1]
+        wavelengths: corresponding wavelengths [nm]
+        lambdas: wavelengths used in QAAV5. Defaults to np.array([440, 490, 555, 640]).
+        k0: Defaults to 6.807 [1].
+        k1: Defaults to 1.186 [1].
+        h0: Defaults to -1.169 [1].
+        h1: Defaults to -1.468 [1].
+        h2: Defaults to 0.274 [1].
+        j_1: Defaults to 0.63 [1].
+        j_2: Defaults to 0.88 [1].
+        a_w_res: absorption coefficient of pure water resampled to the sensors band setting [m-1]. Defaults to [], will be computed if not provided.
+        b_bw_res: backscattering coefficient of pure water resampled to the sensors band setting [m-1]. Defaults to [], will be computed if not provided.
+
+    Returns:
+        a_p_440: particulate absorption coefficient [m-1] at 440 nm
+        a_g_440: CDOM absorption coefficient [m-1] at 440 nm
+    """
+    
+    idx = np.array([find_closest(wavelengths, wl)[1] for wl in lambdas]).astype(int)
+    
+    if len(a_w_res)==0:
+        a_w_res = resample_a_w(wavelengths)
+    if len(b_bw_res)==0:
+        b_bw_res = resample_b_bw(wavelengths)
+
+    # Step 0 [1]
+    r_rs = above2below(R_rs)
+
+    # Step 1 [1]
+    u = 1 - np.exp((-k0 * r_rs**k1)/(0.31 - r_rs))
+
+    # Step 2 [1]
+    chi = np.log((r_rs[idx[0]] + r_rs[idx[1]]) / (r_rs[idx[2]] + 2*(r_rs[idx[3]]/r_rs[idx[2]]) * r_rs[idx[3]]))
+    a_555 = a_w_res[idx[2]] + 10**(h0 + h1*chi + h2*chi**2)
+
+    # Step 3 [1]
+    b_bp_555 = ((u[idx[2]] * a_555) / (1-u[idx[2]])) - b_bw_res[idx[2]]
+    
+    # Step 4 [1]
+    y = 2.2 * (1 - 1.2 * np.exp(-0.9 * (r_rs[idx[0]]/r_rs[idx[2]])))
+    b_bp = np.asarray([b_bp_555 * (wavelengths[idx[2]] / lambda_i)**y for lambda_i in wavelengths])
+    
+    # Step 5 [1]
+    a_440 = (1-u[idx[0]]) * (b_bw_res[idx[0]] + b_bp[idx[0]]) / u[idx[0]]
+
+    # Step 6 [1]
+    a_p_440 = j1 * b_bp[idx[2]]**j2
+
+    # Step 7 [1]
+    a_g_440 = a_440 - a_w_res[idx[0]] - a_p_440
+
+    return a_p_440, a_g_440
