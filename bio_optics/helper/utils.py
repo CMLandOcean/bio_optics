fd03be70f83c3d8ad0136dfef048b2a14f02323b
Marcel König <102319599+mkoenig3@users.noreply.github.com>
2023-06-07 13:55:32 -0700

    added information to estimate_y()

--- bio_optics/helper/utils.py
+++ bio_optics/helper/utils.py
@@ -52,24 +52,26 @@ def band_mask(wavelengths, mask_regions = [[1300,1500],[1800,2000]]):
 
 def estimate_y(R_rs, wavelengths, lambda1=444., lambda2=555., a=2.0, b=1.0, c=1.2, d=-0.9):
     """
-    Two-band estimation of spectral shape parameter of particulate backscattering estimated by an empirical relationship [1,2,3].
-    Default for lambda1 and 2, and coefficients a, b, c, d are from Li et al. (2017) [2].
-    Because it uses a ratio, the input can be in units of R_rs or r_rs.
+    Two-band estimation of spectral shape parameter of particulate backscattering using an empirical relationship [1,2,3] (named Y in [2]).
+    The ratio of the two bands is named chi in [2] and can be in units of R_rs or (subsurface) r_rs.
+    Default for lambda1 and lambda2, and coefficients a, b, c, d are from Li et al. (2017) [4].
+    For lambda1 = 440 and lambda2 = 490 Lee et al. propose a=3.44, b=1, c=3.17, d=-2.01 [2].
+    
     
     [1] Lee et al. (1996): Estimating primary production at depth from remote sensing [10.1364/AO.35.000463]
     [2] Lee et al. (1999): Hyperspectral remote sensing for shallow waters: 2 Deriving bottom depths and water properties by optimization [10.1364/ao.38.003831]
     [3] Lee et al. (2002): Deriving inherent optical properties from water color: A multiband quasi-analytical algorithm for optically deep waters [10.1364/ao.41.005755]
-    [2] Li et al. (2017): Remote sensing estimation of colored dissolved organic matter (CDOM) in optically shallow waters [10.1016/j.isprsjprs.2017.03.015])]
+    [4] Li et al. (2017): Remote sensing estimation of colored dissolved organic matter (CDOM) in optically shallow waters [10.1016/j.isprsjprs.2017.03.015])]
 
     Args:
         R_rs (_type_): Remote sensing reflectance [sr-1]
         wavelengths (_type_): corresponding wavelengths [nm]
         lambda1 (float, optional): wavelength of first band [nm]. Defaults to 444.
         lambda2 (float, optional): wavelength of second band [nm]. Defaults to 555.
-        a (float, optional): _description_. Defaults to 2.
-        b (float, optional): _description_. Defaults to 1.
-        c (float, optional): _description_. Defaults to 1.2.
-        d (float, optional): _description_. Defaults to -0.9.
+        a (float, optional): Defaults to 2.
+        b (float, optional): Defaults to 1.
+        c (float, optional): Defaults to 1.2.
+        d (float, optional): Defaults to -0.9.
 
     Returns:
         y: spectral shape paramter for particulate backscattering coefficient
