7092ded7d3d326f5b96e769e4504917a32fe1dcc
Marcel König <marcel.koenig@brockmann-consult.de>
2024-03-18 10:11:00 +0100

    added glint model

--- bio_optics/models/hereon.py
+++ bio_optics/models/hereon.py
@@ -2,6 +2,8 @@ import numpy as np
 from lmfit import minimize, Parameters
 from .. water import fluorescence, absorption, backscattering, attenuation, scattering, lee
 from .. helper import resampling, utils 
+from .. surface import surface, air_water
+from .. atmosphere import sky_radiance, downwelling_irradiance
 
 def forward(parameters,
             wavelengths,
@@ -24,7 +26,18 @@ def forward(parameters,
             b_d_res=[],
             b_i_spec_res=[],
             c_d_res=[],
-            da_W_div_dT_res=[]):
+            da_W_div_dT_res=[],
+            E_0_res=[],
+            a_oz_res=[],
+            a_ox_res=[],
+            a_wv_res=[],
+            E_dd_res=[],
+            E_dsa_res=[],
+            E_dsr_res=[],
+            E_d_res=[],
+            E_ds_res=[],
+            n2_res=[],
+            Ls_Ed=[]):
     """
     Forward function of the HEREON model described in [1]
 
@@ -56,9 +69,21 @@ def forward(parameters,
 
     Returns:
         _type_: _description_
-    """
-    
+    """    
     C_phy = np.sum([parameters["C_0"], parameters["C_1"], parameters["C_2"], parameters["C_3"], parameters["C_4"], parameters["C_5"]])
+
+    if len(n2_res) == 0:
+        n2 = parameters["n2"]
+    else:
+        n2 = n2_res
+
+    if "rho_L" in parameters:
+        rho_L = parameters["rho_L"].value
+    else:
+        rho_L = air_water.fresnel(parameters["theta_view"], n1=parameters["n1"], n2=n2)
+
+    if len(Ls_Ed) == 0:
+        Ls_Ed = np.zeros_like(wavelengths)
 
     # it makes sense to precompute some coefficients outside of a() and b_b() because they are used in both functions
     if len(b_bw_res)==0:
@@ -204,17 +229,54 @@ def forward(parameters,
                                            b_i_spec_res=b_i_spec_res,
                                            c_d_res=c_d_res)
     
-    R_rs = lee.R_rs_deep(a=a_res, 
-                         b_b=b_b_res, 
-                         b_bp=b_bp_res, 
-                         b_bw=b_bw_res,
-                         Gw0=parameters["Gw0"],
-                         Gw1=parameters["Gw1"],
-                         Gp0=parameters["Gp0"],
-                         Gp1=parameters["Gp1"]) + fluorescence.R_rs_fl(wavelengths=wavelengths,
-                                                                    L_fl_lambda0=parameters['L_fl_lambda0'])
+    R_rs_water = lee.R_rs_deep(a=a_res, 
+                               b_b=b_b_res, 
+                               b_bp=b_bp_res, 
+                               b_bw=b_bw_res,
+                               Gw0=parameters["Gw0"],
+                               Gw1=parameters["Gw1"],
+                               Gp0=parameters["Gp0"],
+                               Gp1=parameters["Gp1"]) + fluorescence.R_rs_fl(wavelengths=wavelengths,
+                                                                             L_fl_lambda0=parameters['L_fl_lambda0'])
     
-    return R_rs
+    if parameters["fit_surface"].value:
+            if len(E_dd_res) == 0:
+                E_dd  = downwelling_irradiance.E_dd(wavelengths, parameters["theta_sun"], parameters["P"], parameters["AM"], parameters["RH"], parameters["H_oz"], parameters["WV"], parameters["alpha"], parameters["beta"], E_0_res, a_oz_res, a_ox_res, a_wv_res, E_dd_res)
+            else:
+                E_dd = E_dd_res
+
+            if len(E_dsa_res) == 0:
+                E_dsa = downwelling_irradiance.E_dsa(wavelengths, parameters["theta_sun"], parameters["P"], parameters["AM"], parameters["RH"], parameters["H_oz"], parameters["WV"], parameters["alpha"], parameters["beta"], E_0_res, a_oz_res, a_ox_res, a_wv_res, E_dsa_res)
+            else:
+                E_dsa = E_dsa_res
+
+            if len(E_dsr_res) == 0:
+                E_dsr = downwelling_irradiance.E_dsr(wavelengths, parameters["theta_sun"], parameters["P"], parameters["AM"], parameters["RH"], parameters["H_oz"], parameters["WV"], parameters["alpha"], parameters["beta"], E_0_res, a_oz_res, a_ox_res, a_wv_res, E_dsr_res)
+            else:
+                E_dsr = E_dsr_res
+
+            if len(E_ds_res) == 0:
+                E_ds = downwelling_irradiance.E_ds(E_dsr, E_dsa)
+            else:
+                E_ds = E_ds_res
+
+            if len(E_d_res) == 0:
+                E_d = downwelling_irradiance.E_d(E_dd, E_ds, parameters["f_dd"], parameters["f_ds"])
+            else:
+                E_d = E_d_res
+
+            L_s = sky_radiance.L_s(parameters["f_dd"], parameters["g_dd"], E_dd, parameters["f_ds"], parameters["g_dsr"], E_dsr, parameters["g_dsa"], E_dsa)
+
+            R_rs_surface = surface.R_rs_surf(L_s, E_d, rho_L, parameters["d_r"])
+
+            R_rs_surface += air_water.fresnel(parameters['theta_view'], n2=n2) * Ls_Ed
+
+            R_rs_sim = R_rs_water + R_rs_surface + parameters["offset"]
+            return R_rs_sim
+        
+    else:
+        R_rs_sim = R_rs_water + parameters["offset"]
+        return R_rs_sim
 
 
 def func2opt(parameters, 
@@ -238,6 +300,17 @@ def func2opt(parameters,
              b_i_spec_res=[],
              c_d_res=[],
              da_W_div_dT_res=[],
+             E_0_res=[],
+             a_oz_res=[],
+             a_ox_res=[],
+             a_wv_res=[],
+             E_dd_res=[],
+             E_dsa_res=[],
+             E_dsr_res=[],
+             E_d_res=[],
+             E_ds_res=[],
+             n2_res=[],
+             Ls_Ed=[],
              omega_d_lambda_0_res=None,
              a_d_lambda_0_res=None,
              c_d_lambda_0_res=None
@@ -299,7 +372,18 @@ def func2opt(parameters,
                        da_W_div_dT_res=da_W_div_dT_res,
                        omega_d_lambda_0_res=omega_d_lambda_0_res,
                        a_d_lambda_0_res=a_d_lambda_0_res,
-                       c_d_lambda_0_res=c_d_lambda_0_res)
+                       c_d_lambda_0_res=c_d_lambda_0_res,
+                       E_0_res=E_0_res,
+                       a_oz_res=a_oz_res,
+                       a_ox_res=a_ox_res,
+                       a_wv_res=a_wv_res,
+                       E_dd_res=E_dd_res,
+                       E_dsa_res=E_dsa_res,
+                       E_dsr_res=E_dsr_res,
+                       E_d_res=E_d_res,
+                       E_ds_res=E_ds_res,
+                       n2_res=n2_res,
+                       Ls_Ed=Ls_Ed)
     
     return utils.compute_residual(R_rs, R_rs_sim, method=parameters['error_method'], weights=weights)
 
@@ -325,6 +409,17 @@ def invert(params,
            b_i_spec_res=[],
            c_d_res=[],
            da_W_div_dT_res=[],
+           E_0_res=[],
+           a_oz_res=[],
+           a_ox_res=[],
+           a_wv_res=[],
+           E_dd_res=[],
+           E_dsa_res=[],
+           E_dsr_res=[],
+           E_d_res=[],
+           E_ds_res=[],
+           n2_res=[],
+           Ls_Ed=[],
            omega_d_lambda_0_res=None,
            a_d_lambda_0_res=None,
            c_d_lambda_0_res=None,
@@ -370,31 +465,97 @@ def invert(params,
     if len(weights)==0:
         weights = np.ones(len(R_rs))
 
-    res = minimize(func2opt, 
-                    params, 
-                    args=(R_rs, 
-                            wavelengths, 
-                            weights,
-                            a_res,
-                            a_d_res,
-                            a_md_spec_res,
-                            a_bd_spec_res,
-                            a_w_res,
-                            a_i_spec_res,
-                            a_phy_res,
-                            a_Y_N_res,
-                            b_b_res,
-                            b_bd_res,
-                            b_bp_res,
-                            b_bphy_res,
-                            b_bw_res,
-                            b_d_res,
-                            b_i_spec_res,
-                            c_d_res,
-                            da_W_div_dT_res,
-                            omega_d_lambda_0_res,
-                            a_d_lambda_0_res,
-                            c_d_lambda_0_res), 
-                    method=method, 
-                    max_nfev=max_nfev) 
+    if params['fit_surface'].value:
+        res = minimize(func2opt, 
+                        params, 
+                        args=(R_rs, 
+                                wavelengths, 
+                                weights,
+                                a_res,
+                                a_d_res,
+                                a_md_spec_res,
+                                a_bd_spec_res,
+                                a_w_res,
+                                a_i_spec_res,
+                                a_phy_res,
+                                a_Y_N_res,
+                                b_b_res,
+                                b_bd_res,
+                                b_bp_res,
+                                b_bphy_res,
+                                b_bw_res,
+                                b_d_res,
+                                b_i_spec_res,
+                                c_d_res,
+                                da_W_div_dT_res,
+                                omega_d_lambda_0_res,
+                                a_d_lambda_0_res,
+                                c_d_lambda_0_res,
+                                E_0_res,
+                                a_oz_res,
+                                a_ox_res,
+                                a_wv_res,
+                                E_dd_res,
+                                E_dsa_res,
+                                E_dsr_res,
+                                E_d_res,
+                                E_ds_res,
+                                n2_res,
+                                Ls_Ed), 
+                        method=method, 
+                        max_nfev=max_nfev) 
+        
+    elif not params['fit_surface'].value:
+        params.add('P', vary=False) 
+        params.add('AM', vary=False) 
+        params.add('RH', vary=False) 
+        params.add('H_oz', vary=False)
+        params.add('WV', vary=False) 
+        params.add('alpha', vary=False) 
+        params.add('beta', vary=False) 
+        params.add('g_dd', vary=False) 
+        params.add('g_dsr', vary=False) 
+        params.add('g_dsa', vary=False) 
+        params.add('f_dd', vary=False) 
+        params.add('f_ds', vary=False) 
+
+        res = minimize(func2opt, 
+                        params, 
+                        args=(R_rs, 
+                                wavelengths, 
+                                weights,
+                                a_res,
+                                a_d_res,
+                                a_md_spec_res,
+                                a_bd_spec_res,
+                                a_w_res,
+                                a_i_spec_res,
+                                a_phy_res,
+                                a_Y_N_res,
+                                b_b_res,
+                                b_bd_res,
+                                b_bp_res,
+                                b_bphy_res,
+                                b_bw_res,
+                                b_d_res,
+                                b_i_spec_res,
+                                c_d_res,
+                                da_W_div_dT_res,
+                                omega_d_lambda_0_res,
+                                a_d_lambda_0_res,
+                                c_d_lambda_0_res,
+                                E_0_res,
+                                a_oz_res,
+                                a_ox_res,
+                                a_wv_res,
+                                E_dd_res,
+                                E_dsa_res,
+                                E_dsr_res,
+                                E_d_res,
+                                E_ds_res,
+                                n2_res,
+                                Ls_Ed), 
+                        method=method, 
+                        max_nfev=max_nfev) 
+
     return res
