4c609c01b40232e5d95b8eea2c9a4017ac0fbb0a
Marcel König <marcel.koenig@brockmann-consult.de>
2024-09-04 08:39:43 +0200

    added adjacency_effect

--- bio_optics/atmosphere/__init__.py
+++ bio_optics/atmosphere/__init__.py
@@ -1,7 +1,8 @@
 __all__ = ["absorption", 
+           "adjacency_effect",
            "downwelling_irradiance", 
            "ET_solar_irradiance", 
            "sky_radiance",
            "transmittance"]
 
-from . import absorption, downwelling_irradiance, ET_solar_irradiance, sky_radiance, transmittance
+from . import absorption, adjacency_effect, downwelling_irradiance, ET_solar_irradiance, sky_radiance, transmittance
